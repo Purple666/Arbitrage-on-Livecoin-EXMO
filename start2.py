f660fd10860496d11299b0bc04b6edf6365106db
topores <36935819+topores@users.noreply.github.com>
2018-03-04 01:53:25 +0300

    Update start2.py

--- start2.py
+++ start2.py
@@ -4,15 +4,15 @@ import time
 
 def getprices():
     response = requests.get('https://api.livecoin.net/exchange/ticker?currencyPair=BTC/USD')
- 
+
     m = response.json()
-   
+
     bestbid1 = m['best_bid']
     bestask1 = m['best_ask']
 
     response = requests.get('https://poloniex.com/public?command=returnTicker')
     m = response.json()
-    
+
     bestbid2 = float(m['USDT_BTC']['highestBid'])
     bestask2 = float(m['USDT_BTC']['lowestAsk'])
 
@@ -25,15 +25,16 @@ def getprices():
 
 def check():
     bid1, ask1, bid2, ask2 = getprices()
-    
-    # arbitrage = 'NO'
-    if ((bid1 / ask2) > 1.01):
+
+
+    if (((bid1 / ask2) > 1.01)):
         arbitrage = 'POLO'
-    elif ((bid2 / ask1) > 1.01):
+    elif (((bid2 / ask1) > 1.01)):
         arbitrage = 'LIVE'
     else:
         arbitrage = 'NO'
-        return arbitrage
+
+    return arbitrage
 
 
 
@@ -51,7 +52,7 @@ def SendMessage(arbitrage):
     bid1, ask1, bid2, ask2 = getprices()
     profitL = (bid1 / ask2 - 1) * 100
     profitE = (bid2 / ask1 - 1) * 100
-
+    print(bid1,' ', ask1,' ',bid2,' ',ask2)
     if (arbitrage == 'NO'):
         bot.send_message(CHANNEL_NAME, 'BTC/USD \nPrepare to close all positions!')
     if (arbitrage == 'LIVE'):
@@ -66,11 +67,15 @@ a2 = 'NO'
 iteration = 0;
 bot.send_message(CHANNEL_NAME, 'Bot Started!')
 while (True):
-    time.sleep(10)
-    a1 = check()
-    if (not a1 == a2):
-        SendMessage(a1)
+    try:
+        time.sleep(10)
+        a1 = check()
+        if (not a1 == a2):
+            SendMessage(a1)
 
-    a2 = a1
-    iteration += 1;
-    print(iteration)
+        a2 = a1
+        iteration += 1;
+        print(iteration)
+    except BaseException:
+        print('Error, check log')
+
